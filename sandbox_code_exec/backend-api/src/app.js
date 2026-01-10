3fd030610c18fd368301357b90a6aa0aacd2b21e
Aryan Yadav <74095206+Sandrocottus1@users.noreply.github.com>
2026-01-10 16:26:27 +0530

    cors updation

--- sandbox_code_exec/backend-api/src/app.js
+++ sandbox_code_exec/backend-api/src/app.js
@@ -1,8 +1,11 @@
 const express=require("express")
 const submitRoutes=require("./routes/submit.routes");
+const cors=require("cors");
+
 
 const app =express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/submit", submitRoutes);
